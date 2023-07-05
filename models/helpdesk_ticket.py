dfde93206902e4d3062b8fa0f88fed35f3ab0732
igruk <igruk_md@ukr.net>
2023-07-05 02:53:02 -0400

    Add Total and Currency fields in Helpdesk ticket

--- models/helpdesk_ticket.py
+++ models/helpdesk_ticket.py
@@ -4,5 +4,7 @@ from odoo import fields, models, api
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
+    total = fields.Float(string='Total')
+    currency = fields.Many2one('res.currency', string='Currency')
     crm_lead_id = fields.Many2one('crm.lead', string='CRM Lead')
 
